7724fbb23f2fbbab6cff7db9aca976521087afdb
Samara-MJ <s.martinezj@uniandes.edu.co>
2023-10-15 22:33:36 -0500

    mapadevehiculosporcategoria

--- Proyecto1_DPOO/Implementacion/src/logica/Categoria.java
+++ Proyecto1_DPOO/Implementacion/src/logica/Categoria.java
@@ -1,9 +1,13 @@
 package logica;
 
+import java.util.Collection;
+import java.util.Map;
+
 public class Categoria {
 	public double iddeCategoria;
 	public double tarifaporDia;
 	public String temporada;
+	Map <String, Vehiculo> listaVehiculos;
 	public Categoria(double iddeCategoria, double tarifaporDia, String temporada) {
 		super();
 		this.iddeCategoria = iddeCategoria;
@@ -18,6 +22,15 @@ public class Categoria {
 	}
 	public String getTemporada() {
 		return temporada;
+	}
+	public Collection<Vehiculo> getListavehiculos(){
+		return this.listaVehiculos.values();
+	}
+	public void agregarVehiculo(String id, Vehiculo elvehiculo) {
+		listaVehiculos.put(id, elvehiculo);
+	}
+	public void setListavehiculos( Map<String, Vehiculo> listaVehiculos) {
+		this.listaVehiculos = listaVehiculos;
 	}
 
 }
